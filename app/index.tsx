45f58cd7a4bf655f8f9e44a6561ed5e88a3399ce
GM-11 <mathuyrgopal@gmail.com>
2025-02-10 13:27:10 +0530

    setting notification handler

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,17 @@
 import { Redirect } from "expo-router";
 import { useAuth } from "../hooks/useAuth";
 import { View, ActivityIndicator } from "react-native";
+import * as Notifications from "expo-notifications";
 
 export default function Index() {
   const { user, loading } = useAuth();
+  Notifications.setNotificationHandler({
+    handleNotification: async () => ({
+      shouldShowAlert: true,
+      shouldPlaySound: true,
+      shouldSetBadge: true,
+    }),
+  });
 
   if (loading) {
     return (
